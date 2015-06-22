33869230eebce2c2ea7b3fa89de0bc950c576da6
Multihuntr <Multihuntr@hotmail.com>
2015-06-22 16:15:23 +1000

    AsteroidSpawns
    Added quick-n-dirty sprite for asteroids.
    Added Asteroid Spawn script to main camera.
    Made Asteroids spawn at a random point on a circle outside the viewport.
    When the asteroid spawns, they are directed at a random point on the
    screen and given a random push in that direction.
    Currently they do not despawn. Probably gonna put something in the
    update for checking position.

---
+++ KineticAstroMiner/Assets/Asteroid.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour
+{
+
+	Rigidbody2D rb;
+	Vector2 aimAt;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		Vector2 twoDPos = gameObject.transform.position;
+		float forceFactor = UnityEngine.Random.value * 10 + 10;
+		rb.AddForce (forceFactor * (aimAt - twoDPos));
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void startRandom ()
+	{
+		// Pick random point in play area
+		float x = UnityEngine.Random.value;
+		float y = UnityEngine.Random.value;
+		aimAt = Camera.main.ViewportToWorldPoint (new Vector2 (x, y));
+	}
+}
