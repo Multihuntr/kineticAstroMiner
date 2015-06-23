8db5d3107d509532d1ea951bf40aab7341b039b3
eyeofstorms <living-lie@hotmail.com>
2015-06-23 12:12:01 +1000

    camera zoom&follow
    added camera zoom, still need to fine tune, follow function correctly,
    needs offsets

---
+++ KineticAstroMiner/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+	public GameObject target;
+	public float xOffset = 0;
+	public float yOffset = 0;
+	private Vector2 velocitytarget;
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+	void LateUpdate ()
+	{
+		Rigidbody2D rigbody = target.GetComponent<Rigidbody2D> ();
+		Vector2 velocity = rigbody.velocity;
+		Camera.main.orthographicSize = Mathf.Lerp (10, 20, Mathf.InverseLerp (0, 20, rigbody.velocity.magnitude));
+		gameObject.transform.position = new Vector3 (target.transform.position.x + xOffset,
+		                                      target.transform.position.y + yOffset, gameObject.transform.position.z);
+	}
+}
--- KineticAstroMiner/Assets/SingleClickMovement.cs
+++ KineticAstroMiner/Assets/SingleClickMovement.cs
@@ -1,28 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SingleClickMovement : MonoBehaviour {
+public class SingleClickMovement : MonoBehaviour
+{
 	public float speed = 15f;
 	private Rigidbody2D rigBody;
 	private Vector2 TargetPosition;
 	private Vector2 CurrentPosition;
-
+	public Vector2 PlayerVelocity;
+	public void velocity ()
+	{
+		PlayerVelocity = rigBody.velocity;
+		Debug.Log ("hi");
+	}
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		rigBody = GetComponent<Rigidbody2D> ();
 		TargetPosition = gameObject.transform.position;
 		CurrentPosition = gameObject.transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	if (Input.GetMouseButtonDown(0)){
-			TargetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			TargetPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			CurrentPosition = gameObject.transform.position;
 		}
-	if (TargetPosition != CurrentPosition) {
-			transform.eulerAngles = new Vector3(0,0,Mathf.Atan2((TargetPosition.y - transform.position.y), (TargetPosition.x - transform.position.x))*Mathf.Rad2Deg - 90);
-			rigBody.AddForce(transform.up * Time.deltaTime * speed);
+		if (TargetPosition != CurrentPosition) {
+			transform.eulerAngles = new Vector3 (0, 0, Mathf.Atan2 ((TargetPosition.y - transform.position.y), (TargetPosition.x - transform.position.x)) * Mathf.Rad2Deg - 90);
+			rigBody.AddForce (transform.up * Time.deltaTime * speed);
 		}
 	}
 }
