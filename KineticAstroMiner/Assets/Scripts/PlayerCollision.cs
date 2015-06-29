9b45c3cc3d0ae35f92bfa99ff8d3eb9b5e4eb4b6
eyeofstorms <living-lie@hotmail.com>
2015-06-30 09:16:27 +1000

    changes
    git says they changed, so PUSH IT i shall

--- KineticAstroMiner/Assets/Scripts/PlayerCollision.cs
+++ KineticAstroMiner/Assets/Scripts/PlayerCollision.cs
@@ -3,17 +3,26 @@ using System.Collections;
 
 public class PlayerCollision : MonoBehaviour
 {
-
+	public float minOmNomAngle;
+	public float maxOmNomAngle;
+	public float asteroidCargo;
+	public float enemyFighterCargo;
 
 	void OnCollisionEnter2D (Collision2D other)
 	{
 		Vector3 targetDir = other.transform.position - gameObject.transform.position;
 		Vector3 forward = transform.up;
 		float angle = Vector3.Angle (targetDir, forward);
-		Debug.Log (angle);
-		if (angle > 0 && angle < 20) {
+		if (angle > minOmNomAngle && angle < maxOmNomAngle) {
 			Destroy (other.gameObject);
+			if (other.gameObject.name == "Asteroid(Clone)") {
+				asteroidCargo += 1;
+			} else if (other.gameObject.name == "enemyFightClone") {
+				enemyFighterCargo += 1;
+			}
+
 		}
+
 	}
 	// Use this for initialization
 	void Start ()
