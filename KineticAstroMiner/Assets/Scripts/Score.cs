5d97ced7480db8f080e1218c3fbb6edd796e846e
Multihuntr <Multihuntr@hotmail.com>
2015-07-02 09:30:16 +1000

    Initial Score class
    Not much to see here.
    Score is pretty simple

---
+++ KineticAstroMiner/Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+
+	// v for value
+	private static int v;
+
+	// Use this for initialization
+	void Start ()
+	{
+		v = 0;
+	}
+
+	public void addScore (int add)
+	{
+		v += add;
+	}
+}
