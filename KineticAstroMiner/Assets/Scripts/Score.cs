423d570606483c68e5f2583aa83fbb3247daa1b6
eyeofstorms <living-lie@hotmail.com>
2015-07-02 12:47:04 +1000

    conflict resolution
    its a skill we all should have

---
+++ KineticAstroMiner/Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+	public GUISkin guiSkin;
+
+	// v for value
+	private static int v;
+
+	void Start ()
+	{
+		v = 0;
+	}
+
+	public void addScore (int add)
+	{
+		v += add;
+	}
+
+	void FixedUpdate ()
+	{
+		v++;
+	}
+
+	void OnGUI ()
+	{
+		GUI.skin = guiSkin;
+		GUI.Label (new Rect (Screen.width - 116, 10, 106, 30), v.ToString ("000000000"));
+	}
+}
