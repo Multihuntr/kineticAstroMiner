186e3a7460f0c98f559a1c21fe06d9d75d34f12c
Multihuntr <Multihuntr@hotmail.com>
2015-07-02 10:52:11 +1000

    Score now displays!
    In a pretty box (actually, the box ain't so pretty)

--- KineticAstroMiner/Assets/Scripts/Score.cs
+++ KineticAstroMiner/Assets/Scripts/Score.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class Score : MonoBehaviour
 {
+	public GUISkin guiSkin;
 
 	// v for value
 	private static int v;
 
-	// Use this for initialization
 	void Start ()
 	{
 		v = 0;
@@ -16,5 +16,16 @@ public class Score : MonoBehaviour
 	public void addScore (int add)
 	{
 		v += add;
+	}
+
+	void FixedUpdate ()
+	{
+		v++;
+	}
+
+	void OnGUI ()
+	{
+		GUI.skin = guiSkin;
+		GUI.Label (new Rect (Screen.width - 116, 10, 106, 30), v.ToString ("000000000"));
 	}
 }
